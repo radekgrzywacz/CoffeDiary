66d19ff12c0b402554b60ae9da255e6064baa7fd
radox9 <grzywaczra@gmail.com>
2024-09-23 01:48:26 +0200

    Input container component for 'Add Coffee' screen.

---
+++ client/components/normalInputContainer.tsx
@@ -0,0 +1,53 @@
+import { View, Text, StyleSheet, TextInput } from "react-native";
+import React from "react";
+import { COLORS } from "../constants/colors";
+import { width } from "../constants/screen";
+
+const InputContainer = ({
+    value,
+    onChange,
+    placeholder,
+}: {
+    value: number;
+    onChange: (text: string) => void;
+    placeholder: string;
+}) => {
+    return (
+        <View style={{ marginTop: 5 }}>
+            <Text style={styles.mediumText}>{placeholder}:</Text>
+            <View style={styles.textInputFrame}>
+                <TextInput
+                    style={styles.textInput}
+                    onChangeText={onChange}
+                    value={value}
+                    autoCapitalize="none"
+                    autoComplete="off"
+                    autoCorrect={false}
+                    placeholder="Name"
+                />
+            </View>
+        </View>
+    );
+};
+
+export default InputContainer;
+
+const styles = StyleSheet.create({
+    mediumText: {
+        fontFamily: "medium",
+        fontSize: 19,
+    },
+    textInputFrame: {
+        borderWidth: 2,
+        padding: 3,
+        borderRadius: 10,
+        borderColor: COLORS.espresso,
+        width: width - 40,
+        height: 40,
+        paddingTop: 5,
+    },
+    textInput: {
+        fontFamily: "regular",
+        fontSize: 19,
+    },
+});
--- client/screens/AddCoffee.tsx
+++ client/screens/AddCoffee.tsx
@@ -3,17 +3,48 @@ import {
     Text,
     SafeAreaView,
     StyleSheet,
+    ScrollView,
     TouchableOpacity,
+    TextInput,
 } from "react-native";
 import React, { useState } from "react";
+import { Feather } from "@expo/vector-icons";
 import { COLORS } from "../constants/colors";
-import { TextInput } from "react-native-gesture-handler";
+import { width } from "../constants/screen";
+import InputContainer from "../components/normalInputContainer";
 
 const AddCoffee = () => {
+    const [name, setName] = useState<string>("");
+    const [country, setCountry] = useState<string>("");
     return (
         <SafeAreaView style={styles.container}>
-            <Text>Add coffee</Text>
-            
+            <ScrollView style={styles.innerContainer}>
+                <TouchableOpacity
+                    style={{ alignSelf: "flex-end", flexDirection: "row" }}
+                    onPress={() => console.log("Saving coffee")}
+                >
+                    <Text
+                        style={{
+                            fontFamily: "light",
+                            fontSize: 15,
+                            color: COLORS.espresso,
+                        }}
+                    >
+                        Save{" "}
+                    </Text>
+                    <Feather name="save" size={20} color={COLORS.espresso} />
+                </TouchableOpacity>
+                <InputContainer
+                    value={name}
+                    onChange={setName}
+                    placeholder="Name"
+                />
+                <InputContainer
+                    value={country}
+                    onChange={setCountry}
+                    placeholder="Country"
+                />
+            </ScrollView>
         </SafeAreaView>
     );
 };
@@ -26,5 +57,9 @@ const styles = StyleSheet.create({
         // alignItems: "center",
         padding: 24,
         backgroundColor: COLORS.almond,
+    },
+    innerContainer: {
+        paddingTop: 10,
+        paddingHorizontal: 20,
     },
 });
